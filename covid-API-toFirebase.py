6570bb53c5bb34388a0ed82886c6d1d34431459c
realmad4 <48329816+realmad4@users.noreply.github.com>
2020-04-26 14:58:01 -0500

    Working py file to upload API data to firebase
    needs exception handling.

--- covid-API-toFirebase.py
+++ covid-API-toFirebase.py
@@ -22,6 +22,7 @@ class countryEntry:
         self.critical = 0
         self.deaths = 0
         self.coordinates = []
+        self.listAvailable = []
 
     # creates a dictionary from json file received from API then hands to data retrieval then links back
     def getAvailableCountries(self):
@@ -36,13 +37,17 @@ class countryEntry:
 
         response = requests.request("GET", url, headers=headers, params=querystring)
         # Dictionary made
-        countryDict = json.loads(response.text)
+        jointCountryDict = json.loads(response.text)
+
         # Parse dict to pass country name into country data request
-        for key in countryDict:
-            newDict: dict = key
-            fullNamesOnly = {k: newDict[k] for k in
-                             newDict.keys() - {'longitude', 'latitude', 'alpha3code', 'alpha2code'}}
+        for key in jointCountryDict:
+            countryDict: dict = key
+            # print(countryDict)
+            fullNamesOnly = {k: countryDict[k] for k in
+                             countryDict.keys() - {'longitude', 'latitude', 'alpha3code', 'alpha2code'}}
             self.country = fullNamesOnly['name']
+            self.listAvailable.append(fullNamesOnly['name'])
+
 
     # Function gets the country name, confirmed cases, recovered cases, critical cases, deaths, and latitude+logitude of
     # that country as JSON file
@@ -50,62 +55,70 @@ class countryEntry:
     def getCountryData(self):
         url = "https://covid-19-data.p.rapidapi.com/country"
 
-        querystring = {"format": "json", "name": self.country}
+        for x in range(len(self.listAvailable)):
+            querystring = {"format": "json", "name": self.listAvailable[x]}
 
-        headers = {
-            'x-rapidapi-host': "covid-19-data.p.rapidapi.com",
-            'x-rapidapi-key': "96c11ee776msh19e72b3b7c301d4p1a4ea7jsnb47d1d1beb7d"
-        }
+            headers = {
+                'x-rapidapi-host': "covid-19-data.p.rapidapi.com",
+                'x-rapidapi-key': "96c11ee776msh19e72b3b7c301d4p1a4ea7jsnb47d1d1beb7d"
+            }
 
-        response = requests.request("GET", url, headers=headers, params=querystring)
+            response = requests.request("GET", url, headers=headers, params=querystring)
 
-        countryData = json.loads(response.text)
-        for key in countryData:
-            countryDict: dict = key
-            print(countryDict)
-            print(type(countryDict))
-            print(self.coordinates)
-            print(int(countryDict['confirmed']))
-            print( int(countryDict['recovered']))
-            print(int(countryDict['critical']))
-            print(int(countryDict['deaths']))
-            print(float(countryDict['latitude']))
-            print(float(countryDict['longitude']))
+            countryData: dict = json.loads(response.text)
+            print(countryData)
+            for key in countryData:
+                # print(key)
+                # print(countryData)
+                # print(key)
+                countryDict: dict = key
+                # print(countryDict)
+                # print(self.coordinates)
+                # print(int(countryDict['confirmed']))
+                # print( int(countryDict['recovered']))
+                # print(int(countryDict['critical']))
+                # print(int(countryDict['deaths']))
+                # print(float(countryDict['latitude']))
+                # print(float(countryDict['longitude']))
 
-        self.confirmed = int(countryDict['confirmed'])
-        self.recovered = int(countryDict['recovered'])
-        self.critical = int(countryDict['critical'])
-        self.deaths = int(countryDict['deaths'])
-        self.coordinates.append(float(countryDict['latitude']))
-        self.coordinates.append(float(countryDict['longitude']))
+                self.country = countryDict['country']
+                self.confirmed = int(countryDict['confirmed'])
+                self.recovered = int(countryDict['recovered'])
+                self.critical = int(countryDict['critical'])
+                self.deaths = int(countryDict['deaths'])
+                self.coordinates.clear()
+                self.coordinates.append(float(countryDict['latitude']))
+                self.coordinates.append(float(countryDict['longitude']))
+
+                self.uploadData()
+
 
     # Set replaces, if we want to simply update instead of replacing our station we can use .
 
+    def uploadData(self):
 
-def uploadData():
-    instance = countryEntry()
-    instance.getAvailableCountries()
-    instance.getCountryData()
+        ref_station_type = db.collection(u'Countries').document(u'' + self.country)
 
-    ref_station_type = db.collection(u'Countries').document(u'' + instance.country)
-
-    countryData = {
-        u'Timestamp': firestore.SERVER_TIMESTAMP,
-        u'Type': u'Feature',
-        u'Geometry': {
-            u'Type': u'Point',
-            u'Coordinates': instance.coordinates,
-            u'Properties': {
-                u'Country name': instance.country,
-                u'Confirmed cases': instance.confirmed,
-                u'Recovered cases': instance.recovered,
-                u'Critical cases': instance.critical,
-                u'Deaths': instance.deaths,
-                u'Coordinates': instance.coordinates,
+        countryData = {
+            u'Timestamp': firestore.SERVER_TIMESTAMP,
+            u'Type': u'Feature',
+            u'Geometry': {
+                u'Type': u'Point',
+                u'Coordinates': self.coordinates,
+                u'Properties': {
+                    u'Country': self.country,
+                    u'Confirmed cases': self.confirmed,
+                    u'Recovered cases': self.recovered,
+                    u'Critical cases': self.critical,
+                    u'Deaths': self.deaths,
+                    u'Coordinates': self.coordinates,
+                }
             }
         }
-    }
-    ref_station_type.set(countryData)
+
+        ref_station_type.set(countryData)
 
 
-uploadData()
+test = countryEntry()
+test.getAvailableCountries()
+test.getCountryData()
